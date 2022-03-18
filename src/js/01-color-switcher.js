2e475b9a64ad9768c1c65b17cc81ac46ed969155
nataliakif <nataliakif@gmail.com>
2022-03-18 15:08:15 +0200

    new

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
-
+const refs ={
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+    body: document.querySelector('body')
+  }
+  
+  refs.startBtn.addEventListener('click', onclickStartButton);
+  refs.stopBtn.addEventListener('click', onclickStopButton);
+  
+   function onclickStartButton(event){
+     const changeColor = getRandomHexColor() ;
+     refs.body.style.backgroundColor = changeColor;
+  }
+  function onclickStopButton(event){
+    const changeColor = getRandomHexColor() ;
+    refs.body.style.backgroundColor = changeColor;
+ }
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+  

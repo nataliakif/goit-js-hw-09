84527a43963e1242e10a73e30e1f670ab8d6cf49
nataliakif <nataliakif@gmail.com>
2022-03-31 15:13:33 +0300

    switcher done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,21 +1,34 @@
+
+let intervalId = null;
+
 const refs ={
     startBtn: document.querySelector('[data-start]'),
     stopBtn: document.querySelector('[data-stop]'),
     body: document.querySelector('body')
   }
-  
+
   refs.startBtn.addEventListener('click', onclickStartButton);
   refs.stopBtn.addEventListener('click', onclickStopButton);
-  
-   function onclickStartButton(event){
-     const changeColor = setTimeout(()=>{getRandomHexColor();
-     refs.body.style.backgroundColor = changeColor}, 1000) 
-  
+  refs.stopBtn.disabled = true;
+
+function onclickStartButton(event){
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+    intervalId = setInterval(changeBackgroundColor, 1000);
+   
+   
 };
   function onclickStopButton(event){
-    const changeColor = getRandomHexColor() ;
-    refs.body.style.backgroundColor = changeColor;
- }
+    refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
+    clearInterval(intervalId);
+ 
+   }
+  function changeBackgroundColor(){
+    refs.body.style.backgroundColor = getRandomHexColor();
+
+  }
+    
   function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
